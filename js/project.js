d067307885dcf66e21eb1b3b2b408d6a98be187b
drasmuss <daniel.rasmussen@appliedbrainresearch.com>
2016-05-22 23:15:14 -0400

    Fix project scrolling

--- js/project.js
+++ js/project.js
@@ -1,16 +1,43 @@
 (function($) {
+    "use strict"; // Start of use strict
 
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    /*
     // sidebar scrolling
     $('#sidebar').affix({
       offset: {
         top: 151
       }
     });
-
+    */
 
     $('#project-top').scrollspy({
         target: '#leftCol',
         offset: 51
     });
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
 
 })(jQuery);
